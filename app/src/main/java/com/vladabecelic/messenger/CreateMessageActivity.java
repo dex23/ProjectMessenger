991df4f44341dc340878c2f5dd6c2f93e2b91179
vladaB91 <vladabecelic@hotmail.com>
2017-03-14 18:36:51 +0100

    Ubacen Intent u CreateMessageActivity

--- app/src/main/java/com/vladabecelic/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/vladabecelic/messenger/CreateMessageActivity.java
@@ -1,6 +1,7 @@
 package com.vladabecelic.messenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +14,7 @@ public class CreateMessageActivity extends Activity {
     }
 
     public void onSendMessage(View view) {
-
+        Intent intent = new Intent(this, RecieveMessageActivity.class);
+        startActivity(intent);
     }
 }
