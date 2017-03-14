b46685768da24ac4a9c967510cf976c5bed4872f
vladaB91 <vladabecelic@hotmail.com>
2017-03-14 17:49:36 +0100

    Updated Manifest and Layouts.

--- app/src/main/java/com/vladabecelic/messenger/RecieveMessageActivity.java
+++ app/src/main/java/com/vladabecelic/messenger/RecieveMessageActivity.java
@@ -1,18 +1,14 @@
 package com.vladabecelic.messenger;
 
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
-public class CreateMessageActivity extends Activity {
+public class RecieveMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_message);
-    }
-
-    public void onSendMessage(View view) {
-
+        setContentView(R.layout.activity_recieve_message);
     }
 }
